7e6e3bec7f488a3e25e08e31a79ee80696b6c055
noe-xyz <169797179+noe-xyz@users.noreply.github.com>
2025-06-09 23:59:45 +0200

    ahora el carrito y pago son dinamico

--- src/Controller/PedidosController.php
+++ src/Controller/PedidosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Cliente;
 use App\Entity\Producto;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,9 +27,22 @@ class PedidosController extends AbstractController
     }
 
     #[Route('/carrito', name: 'carrito')]
-    public function carrito(): Response
+    public function carrito(EntityManagerInterface $entityManager, SessionInterface $session): Response
     {
-        return $this->render('pedidos/carrito.html.twig');
+        $clienteRegistrado = $session->get('nombreCompleto');
+        $cliente = $entityManager->getRepository(Cliente::class)->findOneBy(['nombreCompleto' => $clienteRegistrado]);
+        $domicilioStr = implode(', ', $cliente->getDomicilio());
+        $fechaDeEnvio = $this->calcularFechaEnvio();
+        $subtotal = $this->calcularPrecioTotal($session);
+        $total = $this->calcularPrecioConEnvio($session);
+
+        return $this->render('pedidos/carrito.html.twig', [
+            'cliente' => $cliente,
+            'fechaDeEnvio' => $fechaDeEnvio,
+            'domicilioStr' => $domicilioStr,
+            'subtotal' => $subtotal,
+            'total' => $total,
+        ]);
     }
 
     #[Route('/carrito/add', name: 'add-carrito', methods: ['POST'])]
@@ -54,7 +68,46 @@ class PedidosController extends AbstractController
         return $this->redirectToRoute('carrito');
     }
 
-    //public function calcularPrecioTotal
+    #[Route('/carrito/eliminar/{id}', name: 'eliminar-carrito', methods: ['POST'])]
+    public function eliminarDelCarrito(int $id, Request $request): Response
+    {
+        $carrito = $request->getSession()->get('carrito', []);
+
+        $nuevoCarrito = array_filter($carrito, function ($producto) use ($id) {
+            return $producto['id'] != $id;
+        });
+
+        $request->getSession()->set('carrito', $nuevoCarrito);
+
+        return $this->redirectToRoute('carrito');
+    }
+
+
+    public function calcularPrecioTotal(SessionInterface $session): float
+    {
+        $carritoSesion = $session->get('carrito', []);
+        $precioTotal = 0;
+        foreach ($carritoSesion as $carrito) {
+            $precioTotal += $carrito['precio']; //* $carrito['cantidad'];
+        }
+
+        return $precioTotal;
+    }
+
+    public function calcularPrecioConEnvio(SessionInterface $session): float
+    {
+        $precioTotal = $this->calcularPrecioTotal($session) + 5;
+
+        return $precioTotal;
+    }
+
+    public function calcularFechaEnvio(): \DateTime
+    {
+        $fechaEnvio = new \DateTime();
+        $fechaEnvio->modify('+5 days');
+
+        return $fechaEnvio;
+    }
 
     #[Route('/pago', name: 'pago')]
     public function pago(): Response
@@ -69,8 +122,14 @@ class PedidosController extends AbstractController
     }
 
     #[Route('/pago/confirmacion', name: 'confirmacion')]
-    public function confirmacion(): Response
+    public function confirmacion(SessionInterface $session): Response
     {
-        return $this->render('pedidos/confirmacion.html.twig');
+        $carritoSesion = $session->get('carrito', []);
+        $total = $this->calcularPrecioConEnvio($session);
+
+        return $this->render('pedidos/confirmacion.html.twig', [
+            'carrito' => $carritoSesion,
+            'total' => $total,
+        ]);
     }
 }
